7aff555f8626ec53e9dd9901e1faaa509e99d8b7
Rustikum <133569938+Rustikum@users.noreply.github.com>
2023-07-02 21:36:58 +0300

    Коммит 8го дз

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,48 @@
-//Задача № 1
+//Task № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+
+    function wrapper(...args) {
+        const hash = md5(args) 
+        let objectInCache = cache.find((item) => item.hash === hash); 
+        if (objectInCache) { 
+            console.log("Из кэша: " + objectInCache.value); 
+            return "Из кэша: " + objectInCache.value;
+        }
+    
+        let result = func(...args); 
+        cache.push({hash, value: result}) ; 
+        if (cache.length > 5) { 
+          cache.shift(); 
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;  
+    }
+    return wrapper;
 }
 
-//Задача № 2
+
+//Task № 2
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutId = null;
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+  function wrapper(...args) {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    if (!timeoutId) {
+      func(...args);
+      wrapper.count++;
+    }
+
+    timeoutId = setTimeout(() => {
+      func(...args)
+      wrapper.count++;
+    }, delay);
+
+    wrapper.allCount++;
+  }
+  return wrapper;
 }
